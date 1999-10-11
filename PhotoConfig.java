db52b24b3327bb11aa6132c176e632b5d9b2d7b0
Dustin Sallings <dustin@spy.net>
1999-10-11 04:47:29 +0000

    Get rid of hardcoded references to shite.
    Author: dustin
    Date: 1999/10/10 20:47:29
    
    
    imported from: dustin@spy.net--projects/photo--java--2.0--patch-105

--- PhotoConfig.java
+++ PhotoConfig.java
@@ -1,15 +1,17 @@
 /*
  * Copyright (C) 1999  Dustin Sallings <dustin@spy.net>
  *
- * $Id: PhotoConfig.java,v 1.3 1999/10/10 18:59:59 dustin Exp $
+ * $Id: PhotoConfig.java,v 1.5 1999/10/10 20:47:29 dustin Exp $
  */
 
 public class PhotoConfig extends Object {
 	public String dbDriverName="postgresql.Driver";
-	// public String dbSource="jdbc:postgresql://dhcp-104/photo";
-	public String dbSource="jdbc:postgresql://localhost/photo";
+	public String dbSource="jdbc:postgresql://dhcp-104/photo";
+	// public String dbSource="jdbc:postgresql://localhost/photo";
 	public String dbUser="nobody";
 	public String dbPass="";
+
+	public String objectserver="//dhcp-104/RObjectServer";
 
 	public String cryptohash="SHA";
 }
