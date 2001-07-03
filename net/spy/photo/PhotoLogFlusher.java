fc63485ba7ecc863096e15432a13c0f9867ef664
Dustin Sallings <dustin@spy.net>
2001-07-03 15:56:05 +0000

    Added some debug.
    Author: dustin
    Date: 2001/07/03 07:56:05
    
    
    imported from: dustin@spy.net--projects/photo--java--3.0--patch-182

--- net/spy/photo/PhotoLogFlusher.java
+++ net/spy/photo/PhotoLogFlusher.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1999 Dustin Sallings
  *
- * $Id: PhotoLogFlusher.java,v 1.8 2001/07/03 07:48:52 dustin Exp $
+ * $Id: PhotoLogFlusher.java,v 1.9 2001/07/03 07:56:05 dustin Exp $
  */
 
 package net.spy.photo;
@@ -13,6 +13,7 @@ import java.io.*;
 
 import net.spy.*;
 import net.spy.log.*;
+import net.spy.util.*;
 
 public class PhotoLogFlusher extends SpyLogFlusher {
 
@@ -25,14 +26,17 @@ public class PhotoLogFlusher extends SpyLogFlusher {
 		// Only do all this crap if there's something to log.
 		if(v.size() > 0) {
 			SpyDB photodb=null;
+			Debug debug=new Debug("net.spy.photo.flush.debug");
 			try {
 				photodb = new SpyDB(new PhotoConfig());
 				Connection db=photodb.getConn();
 				Statement st=db.createStatement();
 				for(Enumeration e=v.elements(); e.hasMoreElements();) {
 					SpyLogEntry l=(SpyLogEntry)e.nextElement();
+					debug.debug(l.toString());
 					st.executeUpdate(l.toString());
 				}
+				st.close();
 			} finally {
 				photodb.close();
 			}
