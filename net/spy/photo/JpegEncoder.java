d779354d1b1dbb8523e344de36e780d10d500af9
Dustin Sallings <dustin@spy.net>
2002-02-26 04:06:13 +0000

    Converted this to a UNIX format, and got rid of a redundant field.
    Author: dustin
    Date: 2002/02/25 20:06:13
    
    imported from: dustin@spy.net--projects/photo--java--3.0--patch-303

--- net/spy/photo/JpegEncoder.java
+++ net/spy/photo/JpegEncoder.java
@@ -36,7 +36,7 @@ public class JpegEncoder extends Frame
     int imageHeight, imageWidth;
     int Quality;
     int code;
-    public static int[] jpegNaturalOrder = {
+    public static final int[] jpegNaturalOrder = {
           0,  1,  8, 16,  9,  2,  3, 10,
          17, 24, 32, 25, 18, 11,  4,  5,
          12, 19, 26, 33, 40, 48, 41, 34,
@@ -841,20 +841,6 @@ private class Huffman
     public Vector bits;
     public Vector val;
 
-    /*
-     * jpegNaturalOrder[i] is the natural-order position of the i'th element
-     * of zigzag order.
-     */
-    public final static int[] jpegNaturalOrder = {
-          0,  1,  8, 16,  9,  2,  3, 10,
-         17, 24, 32, 25, 18, 11,  4,  5,
-         12, 19, 26, 33, 40, 48, 41, 34,
-         27, 20, 13,  6,  7, 14, 21, 28,
-         35, 42, 49, 56, 57, 50, 43, 36,
-         29, 22, 15, 23, 30, 37, 44, 51,
-         58, 59, 52, 45, 38, 31, 39, 46,
-         53, 60, 61, 54, 47, 55, 62, 63,
-        };
     /*
     * The Huffman class constructor
     */
