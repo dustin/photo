3e9dbd02ae418021daf7f5be88637ce86001bb57
Dustin Sallings <dustin@spy.net>
2002-05-17 01:07:02 +0000

    New actions for cursor processing.
    Author: dustin
    Date: 2002/05/16 17:07:02
    
    imported from: dustin@spy.net--projects/photo--java--3.0--patch-366

---
+++ net/spy/photo/struts/ResultsAction.java
@@ -0,0 +1,59 @@
+// Copyright (c) 2001  Dustin Sallings <dustin@spy.net>
+//
+// $Id: ResultsAction.java,v 1.1 2002/05/16 17:07:02 dustin Exp $
+
+package net.spy.photo.struts;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import org.apache.struts.action.*;
+
+import net.spy.photo.*;
+
+/**
+ * Action to adjust a cursor before sending it to a result page.
+ */
+public class ResultsAction extends PhotoAction {
+
+	/**
+	 * Get an instance of ResultsAction.
+	 */
+	public ResultsAction() {
+		super();
+	}
+
+    /**
+     * Perform the adjustment (if any).
+     */
+    public ActionForward perform(ActionMapping mapping,
+        ActionForm form,
+        HttpServletRequest request,HttpServletResponse response)
+        throws IOException, ServletException {
+
+        CursorForm cf=(CursorForm)form;
+
+        // Verify there's something to do.
+        if(cf.getStartOffset()!=null && cf.getWhichCursor()!=null) {
+            // Get the session data
+            PhotoSessionData sessionData=getSessionData(request);
+            // Get the cursoor
+            Cursor c=null;
+            if(cf.getWhichCursor().equals("results")) {
+                c=sessionData.getResults();
+            } else if(cf.getWhichCursor().equals("comments")) {
+                c=sessionData.getComments();
+            } else {
+                throw new ServletException(
+                    "Invalid cursor, how did this get through?");
+            }
+
+            // And set the offset.
+            int offset=Integer.parseInt(cf.getStartOffset());
+            c.set(offset);
+        }
+
+        return(mapping.findForward("success"));
+    }
+
+}
