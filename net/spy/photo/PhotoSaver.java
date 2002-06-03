a2abe147bdfdfb5fea733a592cf69ae7be245e9f
Dustin Sallings <dustin@spy.net>
2002-06-03 14:49:21 +0000

    Move the image saving business logic out of the struts action.
    Author: dustin
    Date: 2002/06/03 06:49:21
    
    imported from: dustin@spy.net--projects/photo--java--3.0--patch-416

---
+++ net/spy/photo/PhotoSaver.java
@@ -0,0 +1,199 @@
+// Copyright (c) 2001  Dustin Sallings <dustin@spy.net>
+//
+// $Id: PhotoSaver.java,v 1.1 2002/06/03 06:49:21 dustin Exp $
+
+package net.spy.photo;
+
+import java.sql.*;
+
+import net.spy.SpyDB;
+
+/**
+ * This class is responsible for saving images that have been uploaded.
+ */
+public class PhotoSaver extends Object {
+
+	private String keywords=null;
+	private String info=null;
+	private int cat=-1;
+	private String taken=null;
+	private PhotoUser user=null;
+	private PhotoImage photoImage=null;
+	private Timestamp timestamp=null;
+	private int id=-1;
+
+	/**
+	 * Get an instance of PhotoSaver.
+	 */
+	public PhotoSaver() {
+		super();
+		timestamp=new Timestamp(System.currentTimeMillis());
+	}
+
+	public void setKeywords(String keywords) {
+		this.keywords=keywords;
+	}
+
+	public String getKeywords() {
+		return(keywords);
+	}
+
+	public void setInfo(String info) {
+		this.info=info;
+	}
+
+	public String getInfo() {
+		return(info);
+	}
+
+	public void setCat(int cat) {
+		this.cat=cat;
+	}
+
+	public int getCat() {
+		return(cat);
+	}
+
+	public void setTaken(String taken) {
+		this.taken=taken;
+	}
+
+	public String getTaken() {
+		return(taken);
+	}
+
+	public void setPhotoImage(PhotoImage photoImage) {
+		this.photoImage=photoImage;
+	}
+
+	public PhotoImage getPhotoImage() {
+		return(photoImage);
+	}
+
+	public void setId(int id) {
+		this.id=id;
+	}
+
+	public int getId() {
+		return(id);
+	}
+
+	public void setUser(PhotoUser user) {
+		this.user=user;
+	}
+
+	public PhotoUser getUser() {
+		return(user);
+	}
+
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp=timestamp;
+	}
+
+	public Timestamp getTimestamp() {
+		return(timestamp);
+	}
+
+	/**
+	 * Get a new ID for a photo.
+	 */
+	public static int getNewImageId() throws PhotoException {
+		int rv=0;
+		try {
+			SpyDB db=new SpyDB(new PhotoConfig());
+			ResultSet rs=db.executeQuery("select nextval('album_id_seq')");
+			if(!rs.next()) {
+				throw new PhotoException("No result for new album ID");
+			}
+			rv=rs.getInt(1);
+			if(rs.next()) {
+				throw new PhotoException("Too many results for new album ID");
+			}
+			rs.close();
+			db.close();
+		} catch(PhotoException e) {
+			throw e;
+		} catch(Exception e) {
+			throw new PhotoException("Error getting image ID", e);
+		}
+		return(rv);
+	}
+
+	/**
+	 * Tell the PhotoSaver it will not be needed soon.
+	 */
+	public void passivate() {
+	}
+
+	/**
+	 * Tell the PhotoSaver to active itself for saving.
+	 */
+	public void activate() {
+	}
+
+	/**
+	 * Activate and perform this save.
+	 */
+	public void saveImage() throws PhotoException {
+		activate();
+
+		if(id==-1) {
+			throw new PhotoException("ID not set.");
+		}
+
+		SpyDB db=null;
+		Connection conn=null;
+		try {
+			String query=null;
+			db=new SpyDB(new PhotoConfig());
+			conn=db.getConn();
+			conn.setAutoCommit(false);
+
+			query = "insert into album(id, keywords, descr, cat, taken, size, "
+				+ " addedby, ts, width, height)\n"
+				+ "   values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+			PreparedStatement pst=conn.prepareStatement(query);
+
+			int i=1;
+			pst.setInt(i++, getId());
+			pst.setString(i++, getKeywords());
+			pst.setString(i++, getInfo());
+			pst.setInt(i++, getCat());
+			pst.setString(i++, getTaken());
+			pst.setInt(i++, getPhotoImage().size());
+			pst.setInt(i++, user.getId());
+			pst.setTimestamp(i++, getTimestamp());
+			pst.setInt(i++, getPhotoImage().getWidth());
+			pst.setInt(i++, getPhotoImage().getHeight());
+
+			// Run the query
+			pst.executeUpdate();
+
+			// Cache the image data
+			PhotoImageHelper photo_helper=new PhotoImageHelper(getId());
+			photo_helper.storeImage(getPhotoImage());
+
+			// Image is stored, commit.
+			conn.commit();
+
+		} catch(Exception e) {
+			try {
+				conn.rollback();
+			} catch(Exception e2) {
+				e2.printStackTrace();
+			}
+			throw new PhotoException("Error saving image.", e);
+		} finally {
+			if(conn!=null) {
+				try {
+					conn.setAutoCommit(true);
+					db.close();
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		} // Finally block
+	}
+
+}
--- net/spy/photo/struts/UploadAction.java
+++ net/spy/photo/struts/UploadAction.java
@@ -1,6 +1,6 @@
 // Copyright (c) 2001  Dustin Sallings <dustin@spy.net>
 //
-// $Id: UploadAction.java,v 1.2 2002/05/18 07:30:11 dustin Exp $
+// $Id: UploadAction.java,v 1.3 2002/06/03 06:49:22 dustin Exp $
 
 package net.spy.photo.struts;
 
@@ -28,92 +28,48 @@ public class UploadAction extends PhotoAction {
 		super();
 	}
 
-	/**
-	 * Process the upload.
-	 */
 	public ActionForward perform(ActionMapping mapping,
 		ActionForm form,
 		HttpServletRequest request,HttpServletResponse response)
 		throws IOException, ServletException {
 
 		UploadForm uf=(UploadForm)form;
-
 		// Get the session data.
 		PhotoSessionData sessionData=getSessionData(request);
-
 		// Verify upload permission.
 		int cat=Integer.parseInt(uf.getCategory());
-		if(!sessionData.getUser().canAdd(cat)) {
+
+		// Get the user, and verify he/she can add to the requested category
+		PhotoUser user=sessionData.getUser();
+		if(!user.canAdd(cat)) {
 			throw new ServletException(
 				sessionData.getUser() + " can't add to category " + cat);
 		}
 
-		// Save the image.
-		SpyDB db=null;
-		Connection conn=null;
 		try {
-			String query=null;
 
-			db=new SpyDB(new PhotoConfig());
-			conn=db.getConn();
-			conn.setAutoCommit(false);
+			// Get the ID
+			int id=PhotoSaver.getNewImageId();
+			PhotoSaver saver=new PhotoSaver();
 
-			// First build the query to store the image.
-			query = "insert into album(keywords, descr, cat, taken, size, "
-				+ " addedby, ts, width, height)\n"
-				+ "   values(?, ?, ?, ?, ?, ?, ?, ?, ?)";
-			PreparedStatement st=conn.prepareStatement(query);
+			saver.setKeywords(uf.getKeywords());
+			saver.setInfo(uf.getInfo());
+			saver.setCat(cat);
+			saver.setTaken(uf.getTaken());
+			saver.setUser(user);
+			saver.setPhotoImage(uf.getPhotoImage());
+			saver.setId(id);
 
-			st.setString(1, uf.getKeywords());
-			st.setString(2, uf.getInfo());
-			st.setInt(3, Integer.parseInt(uf.getCategory()));
-			st.setString(4, uf.getTaken());
-			st.setInt(5, uf.getPhotoImage().size());
-			st.setInt(6, sessionData.getUser().getId());
-			st.setTimestamp(7,
-				new java.sql.Timestamp(System.currentTimeMillis()));
-			st.setInt(8, uf.getPhotoImage().getWidth());
-			st.setInt(9, uf.getPhotoImage().getHeight());
-			st.executeUpdate();
+			saver.saveImage();
 
-			// Get the image ID
-			query = "select currval('album_id_seq')";
-			ResultSet rs = st.executeQuery(query);
-			rs.next();
-			int id=rs.getInt(1);
-
-			// Store the actual image data.
-			PhotoImageHelper photo_helper=new PhotoImageHelper(id);
-			photo_helper.storeImage(uf.getPhotoImage());
-
-			// Image is stored, commit.
-			conn.commit();
-
-			// Log the new image
 			Persistent.logger.log(new PhotoLogUploadEntry(
-				sessionData.getUser().getId(), id, request));
+				user.getId(), id, request));
 
 			// Before we return, make the ID available to the next handler
 			request.setAttribute("net.spy.photo.UploadID", new Integer(id));
-
-		} catch(Exception e) {
-			e.printStackTrace();
-			try {
-				conn.rollback();
-			} catch(Exception e2) {
-				e2.printStackTrace();
-			}
-		} finally {
-			if(conn!=null) {
-				try {
-					conn.setAutoCommit(true);
-					db.close();
-				} catch(Exception e) {
-					e.printStackTrace();
-				}
-			}
+		} catch(PhotoException e) {
+			throw new ServletException("Error saving image.", e);
 		}
-
 		return(mapping.findForward("success"));
 	}
 
