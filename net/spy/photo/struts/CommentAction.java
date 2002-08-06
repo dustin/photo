3f958d0a4c0b14c402c943a082e488fb5d934567
Dustin Sallings <dustin@spy.net>
2002-08-06 11:37:08 +0000

    retabbed
    Author: dustin
    Date: 2002/08/06 03:37:08
    
    imported from: dustin@spy.net--projects/photo--java--3.0--patch-587

--- net/spy/photo/struts/CommentAction.java
+++ net/spy/photo/struts/CommentAction.java
@@ -1,6 +1,6 @@
 // Copyright (c) 2001  Dustin Sallings <dustin@spy.net>
 //
-// $Id: CommentAction.java,v 1.2 2002/07/10 03:38:09 dustin Exp $
+// $Id: CommentAction.java,v 1.3 2002/08/06 03:37:08 dustin Exp $
 
 package net.spy.photo.struts;
 
@@ -42,42 +42,47 @@ public class CommentAction extends PhotoAction {
 
 		CommentForm cf=(CommentForm)form;
 
-        // Get the session data
-        PhotoSessionData sessionData=getSessionData(request);
-        // Get the user
-        PhotoUser user=sessionData.getUser();
+		// Get the session data
+		PhotoSessionData sessionData=getSessionData(request);
+		// Get the user
+		PhotoUser user=sessionData.getUser();
 
-        int imageId=Integer.parseInt(cf.getImageId());
+		int imageId=Integer.parseInt(cf.getImageId());
 
-        // Check permission
-        try {
-            Persistent.getSecurity().checkAccess(user, imageId);
-        } catch(Exception e) {
-            throw new ServletException(e.getMessage(), e);
-        }
+		// Check permission
+		try {
+			Persistent.getSecurity().checkAccess(user, imageId);
+		} catch(Exception e) {
+			throw new ServletException(e.getMessage(), e);
+		}
 
-        // Construct the comment.
-        Comment comment=new Comment();
-        comment.setUser(user);
-        comment.setPhotoId(imageId);
-        comment.setRemoteAddr(request.getRemoteAddr());
-        comment.setNote(cf.getComment());
+		// Construct the comment.
+		Comment comment=new Comment();
+		comment.setUser(user);
+		comment.setPhotoId(imageId);
+		comment.setRemoteAddr(request.getRemoteAddr());
+		comment.setNote(cf.getComment());
 
-        try {
-            comment.save();
-        } catch(Exception e) {
-            throw new ServletException("Error saving comment", e);
-        }
+		try {
+			comment.save();
+		} catch(Exception e) {
+			throw new ServletException("Error saving comment", e);
+		}
 
-        // Get the configured forward
-        ActionForward forward=mapping.findForward("success");
-        // If we got one, modify it to include the image ID.
-        if(forward!=null) {
-            String path=forward.getPath();
-            path+="?id=" + imageId;
-            forward.setPath(path);
-        }
-		return(forward);
+		ActionForward rv=null;
+
+		// Get the configured forward
+		ActionForward forward=mapping.findForward("success");
+		// If we got one, modify it to include the image ID.
+		if(forward!=null) {
+			String path=forward.getPath();
+			path+="?id=" + imageId;
+			// Duplicate the forward since modifying it does bad things.
+			rv=new ActionForward(path);
+			rv.setName(forward.getName());
+			rv.setRedirect(forward.getRedirect());
+		}
+		return(rv);
 	}
 
 }
