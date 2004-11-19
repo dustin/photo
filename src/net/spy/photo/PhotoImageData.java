b594140eec7f7c8d4c7391c2cf7f2b89b3da6049
Dustin Sallings <dustin@spy.net>
2004-11-19 01:44:19 +0000

    Document PhotoImageData.
    imported from: dustin@spy.net--projects-2004/photo--java--3.2--patch-10

--- src/net/spy/photo/PhotoImageData.java
+++ src/net/spy/photo/PhotoImageData.java
@@ -7,26 +7,59 @@ package net.spy.photo;
  */
 public interface PhotoImageData {
 
+	/** 
+	 * Get the keywords for this image (space separated).
+	 */
 	String getKeywords();
 
+	/** 
+	 * Get the description of this image.
+	 */
 	String getDescr();
 
+	/** 
+	 * Get the ID of the category of this image.
+	 */
 	int getCatId();
 
+	/** 
+	 * Get the size (in bytes) of this image.
+	 */
 	int getSize();
 
+	/** 
+	 * Get the dimensions of this image.
+	 */
 	PhotoDimensions getDimensions();
 
+	/** 
+	 * Get the dimensions of the thumbnail of this image.
+	 */
 	PhotoDimensions getTnDims();
 
+	/** 
+	 * Get the user who added this image.
+	 */
 	PhotoUser getAddedBy();
 
+	/** 
+	 * Get the timestamp of this image.
+	 */
 	String getTimestamp();
 
+	/** 
+	 * Get the name of the category of this image.
+	 */
 	String getCatName();
 
+	/** 
+	 * Get the date this image was taken/created (yyyy-MM-dd).
+	 */
 	String getTaken();
 
+	/** 
+	 * Get the ID of this image.
+	 */
 	int getId();
 
 }
