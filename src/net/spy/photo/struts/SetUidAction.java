1994f15a571c51eda83cbc99121c26641082d720
Dustin Sallings <dustin@spy.net>
2005-02-06 14:49:43 +0000

    Added setuid functionality.
    imported from: dustin@spy.net--projects-2005/photo--main--3.3--patch-30

---
+++ src/net/spy/photo/struts/SetUidAction.java
@@ -0,0 +1,55 @@
+// Copyright (c) 2004  2Wire, Inc.
+// arch-tag: D2264AC5-7808-11D9-87B0-000A957659CC
+
+package net.spy.photo.struts;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.DynaActionForm;
+
+import net.spy.photo.Persistent;
+import net.spy.photo.log.PhotoLogEntry;
+import net.spy.photo.PhotoSessionData;
+import net.spy.photo.User;
+import net.spy.photo.UserFactory;
+
+/**
+ * Struts action for a user with admin role to become another user.
+ */
+public class SetUidAction extends PhotoAction {
+
+	/**
+	 * Get an instance of SetUidAction.
+	 */
+	public SetUidAction() {
+		super();
+	}
+
+	/** 
+	 * Perform this action.
+	 */
+	public ActionForward spyExecute(ActionMapping mapping, ActionForm form,
+		HttpServletRequest request, HttpServletResponse response)
+		throws Exception {
+
+		PhotoSessionData sessionData=getSessionData(request);
+		User thisUser=sessionData.getUser();
+		if(!thisUser.isInRole("admin")) {
+			throw new ServletException("You are not an admin");
+		}
+
+		DynaActionForm lf=(DynaActionForm)form;
+
+		User thatUser=Persistent.getSecurity().getUser((String)lf.get("user"));
+		sessionData.setUser(thatUser);
+		getLogger().info("User " + thisUser + " has become " + thatUser);
+
+		return(mapping.findForward("next"));
+	}
+
+}
