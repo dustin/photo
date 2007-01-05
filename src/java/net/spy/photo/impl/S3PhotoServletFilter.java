66699f51bd0a50df93a1cedc4689842dd56114f3
Dustin Sallings <dustin@spy.net>
2007-01-04 22:39:32 -0800

    Separate out the Cacher task.  I kind of liked it as an anonymous inner, but I wanted to avoid the synthetic accessor which would lead to slightly confusing logging.

--- src/java/net/spy/photo/impl/S3PhotoServletFilter.java
+++ src/java/net/spy/photo/impl/S3PhotoServletFilter.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.spy.SpyObject;
 import net.spy.photo.Persistent;
 import net.spy.photo.PhotoDimensions;
 import net.spy.photo.PhotoImageData;
@@ -14,32 +15,44 @@ import net.spy.photo.User;
 
 public class S3PhotoServletFilter extends BasePhotoServletFilter {
 
-	public void doFilter(final PhotoImageData pid, User u,
-			final PhotoDimensions dims,
+	public void doFilter(PhotoImageData pid, User u, PhotoDimensions dims,
 			HttpServletRequest req, HttpServletResponse res,
 			PhotoServletChain chain) throws Exception {
 
-		boolean local=req.getRemoteAddr().startsWith("192.168.");
+		boolean local = req.getRemoteAddr().startsWith("192.168.");
 
-		final S3Service s3s=S3Service.getInstance();
-		if(!local && s3s.contains(pid.getId(), dims, pid.getFormat())) {
-			String url=s3s.makeURL(pid.getId(), dims, pid.getFormat());
+		S3Service s3s = S3Service.getInstance();
+		if (!local && s3s.contains(pid.getId(), dims, pid.getFormat())) {
+			String url = s3s.makeURL(pid.getId(), dims, pid.getFormat());
 			getLogger().debug("Redirecting to %s", url);
 			res.sendRedirect(url);
 		} else {
-			if(!local && dims != null && s3s.isFunctional()) {
-				Persistent.getExecutor().schedule(new Runnable() {
-					public void run() {
-						try {
-							s3s.send(pid, dims);
-						} catch(Exception e) {
-							getLogger().warn("Problem sending missing image",
-									e);
-						}}},
-						5, TimeUnit.SECONDS);
+			if (!local && dims != null && s3s.isFunctional()) {
+				Persistent.getExecutor().schedule(new Cacher(pid, dims),
+					5, TimeUnit.SECONDS);
 			}
 			chain.doChain(pid, u, dims, req, res);
 		}
 	}
 
+	// This is really just a little lambda class, except for the exception
+	// handling and then having to send some more data in because it's not
+	// a closure like it used to be and stuff like that.
+	private static class Cacher extends SpyObject implements Runnable {
+		private PhotoImageData pid = null;
+		private PhotoDimensions dims = null;
+
+		public Cacher(PhotoImageData p, PhotoDimensions d) {
+			super();
+			pid = p;
+			dims = d;
+		}
+		public void run() {
+			try {
+				S3Service.getInstance().send(pid, dims);
+			} catch (Exception e) {
+				getLogger().warn("Problem sending missing image", e);
+			}
+		}
+	}
 }
